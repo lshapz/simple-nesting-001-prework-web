1f7d049cfb349c702880e5bf0baec2d7420f5587
lshapz <likeaduck@gmail.com>
2016-06-27 13:54:15 -0400

    Done.

--- nested.rb
+++ nested.rb
@@ -16,7 +16,7 @@ def hopper
         }
      }
 
-
+return programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -37,6 +37,7 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     return programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,6 +56,10 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
+a = programmer_hash[:dennis_ritchie][:languages]
+b = a.join 
+return b
 end
 
 def adding_matz
@@ -80,7 +85,8 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+return programmer_hash
 end
 
 def changing_alan
@@ -101,7 +107,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash   
      
 end
 
@@ -122,6 +129,8 @@ def adding_to_dennis
         }
      }
 
+programmer_hash[:dennis_ritchie][:languages].push ("Assembly")
+return programmer_hash
      
 end
 
